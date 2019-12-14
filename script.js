fd169ec330b6884c14d04aa24128995072209d8c
Jan Reinhardt <Jan.Reinhardt@aitgmbh.de>
2019-12-15 00:09:58 +0100

    button 13 & 14

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ element.addEventListener("click", function(e){
   void element.offsetWidth;
   element.classList.add("button6");
 }, false);
+
+var clickSoundButton = document.getElementsByClassName("button14")[0];
+clickSoundButton.addEventListener("mousedown", function(e){
+  var audio = new Audio('switchSound.wav');
+  audio.play();
+}, false)
+
+clickSoundButton.addEventListener("mouseup", function(e){
+  var audio = new Audio('switchSound.wav');
+  audio.play();
+}, false)
